7b127f7db3fd2c41a93eae945c852c54fe81f803
Jan Zborowski <janzborowski99@gmail.com>
2021-04-28 13:54:23 +0200

    Setup eslint and prettier

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,20 @@
+import express, { Request, Response } from "express";
+
 import { createConnection } from "typeorm";
-import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-createConnection().then((_connection) => {
+const main = async () => {
+  await createConnection();
+
   app.use(express.json());
 
+  app.get("/healthcheck", (_req: Request, res: Response) => {
+    res.json({ ok: "ok" });
+  });
+
   app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
-});
+};
+
+main().catch(console.error);

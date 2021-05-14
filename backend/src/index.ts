507949f6f0192e82f36471e2974bad8e14371d8c
Jan Zborowski <janzborowski99@gmail.com>
2021-05-14 16:08:17 +0200

    Add sessions

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,25 @@
 import express, { Request, Response } from "express";
 
+import connectPg from "connect-pg-simple";
 import cors from "cors";
 import { createConnection } from "typeorm";
 import helmet from "helmet";
 import morgan from "morgan";
+import pg from "pg";
 import router from "./api/routes";
+import session from "express-session";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+const pgPool = new pg.Pool({
+  host: process.env.POSTGRES_HOST || "localhost",
+  port: parseInt(process.env.POSTGRES_PORT ?? "5432"),
+  user: process.env.POSTGRES_USER || "root",
+  password: process.env.POSTGRES_PASSWORD || "root",
+  database: process.env.POSTGRES_DB || "blood_centers",
+});
+const pgSession = connectPg(session);
 
 const main = async () => {
   await createConnection();
@@ -16,6 +28,21 @@ const main = async () => {
   app.use(helmet());
   app.use(morgan("short"));
   app.use(cors());
+  app.use(
+    session({
+      name: "session_cookie",
+      store: new pgSession({
+        pool: pgPool,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+        httpOnly: true,
+      },
+      secret: process.env.SESSION_SECRET || "secret",
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
   app.use(router);
 
   app.get("/healthcheck", (_req: Request, res: Response) => {

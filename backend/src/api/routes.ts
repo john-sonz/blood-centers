734418c66b27be8f0402b82e7f7e9c4825dd565d
kingasakol <68615103+kingasakol@users.noreply.github.com>
2021-05-15 12:18:59 +0200

    add entities

--- backend/src/api/routes.ts
+++ backend/src/api/routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import users from "./users";
+import donations from "./donations";
+import receipts from "./receipts";
 
 const router = Router();
 
 router.use("/users", users);
+
+router.use("/donations", donations);
+
+router.use("/receipts", receipts);
 
 export default router;
